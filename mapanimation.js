73edc637d27b642f0c575fbb0a395b328e2028a0
J0J0 Todos <jojo@peek-a-boo.at>
2023-09-30 19:25:59 +0200

    Move main js code to mapanimation.js

---
+++ mapanimation.js
@@ -0,0 +1,48 @@
+async function initMap() {
+  // Request needed libraries.
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+  // Create map, centered at position
+  const position = {lat:42.353350, lng:-71.091525};
+  map = new Map(document.getElementById("map"), {
+    zoom: 14,
+    center: position,
+		mapTypeId : google.maps.MapTypeId.ROADMAP,
+    mapId: "JT_MITX_BUS",
+  });
+  // Start main program
+  putMarkers();
+
+}
+
+
+async function putMarkers(){
+  // get bus data
+	const locations = await getBusLocations();
+
+  locations.forEach((bus) => {
+    console.log(bus.id);
+    // The marker, positioned at Uluru
+    // const marker = new AdvancedMarkerElement({
+    //   map: map,
+    //   position: position,
+    //   title: "Uluru",
+    // });
+  })
+	console.log(new Date().toISOString());
+	console.log(locations);
+	setTimeout(putMarkers, 15000);
+}
+
+// Request bus data from MBTA
+async function getBusLocations(){
+	const url = 'https://api-v3.mbta.com/vehicles?filter[route]=1&include=trip';
+	const response = await fetch(url);
+	const json     = await response.json();
+	return json.data;
+}
+
+
+let map;  // Initialize and add the map
+var markers = []; // Save markers in this array
+initMap();
